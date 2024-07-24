4eda579b37916486f509f36ac9aa68ee590d8f14
John <anuarbek-dauren@mail.ru>
2024-07-24 21:59:31 +0600

    БД для фильмов

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
-let fugure = 1;
-console.log(fugure);
-fugure = 2
-console.log(fugure);
+// let fugure = 1;
+// console.log(fugure);
+// fugure = 2
+// console.log(fugure);
+
+// const result = confirm("are you OK?");
+// const answer = prompt("How old are you?", " ");
+// console.log(typeof(answer));
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", " ");
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+
+const a = prompt("Один из последних просмотренных фильмов?", " "),
+      b = prompt("На сколько оцените его?", " "),
+      c = prompt("Один из последних просмотренных фильмов?", " "),
+      d = prompt("На сколько оцените его?", " ");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
